be3858c344b79247c381f25b5d40055fd1e5a77d
Ross Thompson <rossathompson94@gmail.com>
2020-02-08 19:16:44 +0000

    added in react-router and implemented basic navigation in a new component called header with 3 unique routes. added in some basic styling for Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'; 
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+  } from 'react-router-dom';
 
 // Styles
 import './App.scss'
 
 // Components
+import Header from './components/Header/Header'
 import JokeDisplay from './components/JokeDisplay'
 import Button from './components/Button'
 
@@ -21,39 +27,26 @@ class App extends React.Component {
     }
 
 
-    _callRandomJoke = async () => {
+    callRandomJoke = async () => {
         await chuckAPI.get(`/jokes/random`)
         .then(res => this.setState({randomJoke: res.data.value.joke}))
+        .catch(err => console.log(err))
     };
-
-    // 
-
-    // onSearchSubmit = async (term) =>  {
-    //     if(term.split(" ").length > 1) {
-    //       await owm.get(`?q=${term.split(" ")[0]},${term.split(" ")[1]}&units=metric&APPID=${this.state.API_key}`).then(res => this.setState({weather:res.data, base_data:res, resStatus: ''}))
-    //       .catch(err => err ? this.setState({resStatus:"unsuccessful", base: '', weather: ''}) : null)
-    //     } else {
-    //       await owm.get(`?q=${term}&units=metric&APPID=${this.state.API_key}`).then(res => this.setState({weather:res.data, base_data:res, resStatus: ''}))
-    //       .catch(err => err ? this.setState({resStatus:"unsuccessful", base: '', weather: ''}) : null)
-    //     }
-    
-    //     console.log(this.state.weather);
-    //   }
 
     render() {
         return  (
-            <div className="App">
-                <div className="title">
-                    <h1 className="appHeading">ReactJS Joke App</h1>
-                </div>
-                <Button
-                    onClick={this._callRandomJoke}
-                    buttonText="Random Joke"
-                />
-                <JokeDisplay
-                    jokeResult={this.state.randomJoke}
-                />        
-            </div>         
+            <Router>
+                <div className="App">
+                    <Header />
+                    {/* <Button
+                        onClick={this.callRandomJoke}
+                        buttonText="Random Joke"
+                    />
+                    <JokeDisplay
+                        jokeResult={this.state.randomJoke}
+                    />         */}
+                </div>  
+            </Router>       
         )
     };
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+          <div className="logo">
+            <Link to='/Random-joke'>React App</Link>
+          </div>
+          <div className="navigation">
+            <ul className="nav-ul">
+              <li className='nav-li'><Link to='/Random-joke'>Random Joke</Link></li>
+              <li className="nav-li"><Link to='/Search-joke'>Search Joke</Link></li>
+              <li className="nav-li"><Link to='/Joke-list'>Joke-list</Link></li>
+            </ul>
+          </div>      
+      </header>
+    )
+  }
+}
+
+export default Header;
